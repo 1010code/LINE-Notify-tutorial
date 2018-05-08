ea1c7c2e6c653c4b32c33806d9f92a679c3e1805
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-05-08 16:20:03 +0800

    add API

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const lineNotify = require('express-line-notify');
 const config = {
-  clientId: 'qPWMrOPQREqdEdajUMz2qo',
-  clientSecret: 'OnVjSHAxnGhG2c5WOhpaSXbMZ2J1NgbKMQWqrV4etsy',
+  clientId: 'qNdZpKv9GVGmdPDGgMlXJZ',
+  clientSecret: 'iCR5qokHNOq8oHMsOgnp7N9Z1uzxdrFJ6OBW04hXouK',
 };
 const app = express();
-app.use(
-  '/',
-  lineNotify(config)
-);
+app.use('/',
+  function (req, res, next) {
+    req['context'] = req.query.data;    //store whatever in query string
+    next();
+  },
+  lineNotify(config),
+  function (req, res) {
+    const token = req['line-notify-access-token'];
+    const data = req['context']    //data will be { userid: "123", email: "456" }
+    console.log(token);
+    console.log(data);
+    res.send(token);
+  });
 app.listen(3000);
